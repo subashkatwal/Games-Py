a981ffafd03b422cde80394710861e74b4f45261
subashkatwal <143714986+subashkatwal@users.noreply.github.com>
2023-09-18 09:47:52 +0545

    Add files via upload

---
+++ user_login.py
@@ -0,0 +1,22 @@
+attempts = 3
+
+while attempts > 0:
+    email = input("Give your email address: ")
+
+    if '@' in email:
+        password = input("Give your password: ")
+
+        if email == "subash@gmail.com" and password == "1234":
+            print("Welcome!")
+            break  # Exit the loop if both email and password are correct
+        elif email == "subash@gmail.com":
+            print("Password incorrect. You have", attempts - 1, "attempts remaining.")
+        else:
+            print("Email is correct, but the password is incorrect. You have", attempts - 1, "attempts remaining.")
+    else:
+        print("Email is incorrect - missing '@' symbol")
+
+    attempts -= 1
+
+if attempts == 0:
+    print("Exceeded maximum login attempts. Exiting.")
